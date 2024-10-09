e065830ef5bb4f3415f473cd440b1c4d67592b32
AdikaNathaniel <128187291+AdikaNathaniel@users.noreply.github.com>
2024-10-09 15:23:07 +0000

    Newest Commit

---
+++ CSharpPractice/NathanPractice/FitnessApp/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello, You");
+
+
+Console.WriteLine("Please Enter Your First Name");
+string firstName = Console.ReadLine();
+
+
+Console.WriteLine("Please Enter Your Last Name");
+string lastName = Console.ReadLine();
+
+
+Console.WriteLine("Hello " +firstName + " " +lastName);
